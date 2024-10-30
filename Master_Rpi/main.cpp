4d8e366d722e4cb03af8e63e47d36d36c050cbf1
schmidgall <sara.schmidgall@googlemail.com>
2024-10-30 14:55:44 +0100

    version control

--- Master_Rpi/main.cpp
+++ Master_Rpi/main.cpp
@@ -73,14 +73,14 @@ uint16_t ModRTU_CRC(uint8_t buf[], int len)
    return crc;
 }
 
-int sendMessage(int argc, char *argv[])
+int sendMessage(stMessage send_msg)
 {
    int file, count;
 
-   if(argc!=5){
-       printf("Invalid number of arguments, exiting [%d]!\n", argc);
-       return -2;
-   }
+//    if(len(send_msg)!=5){
+//        printf("Invalid number of arguments, exiting [%d]!\n", argc);
+//        return -2;
+//    }
    if ((file = open("/dev/ttyS0", O_RDWR | O_NOCTTY))<0) // remove O_NDELAY
    {
       perror("UART: Failed to open the file.\n");
@@ -96,27 +96,31 @@ int sendMessage(int argc, char *argv[])
    tcflush(file, TCIFLUSH); // discard file information
    tcsetattr(file, TCSANOW, &options);
 
-   const size_t MSG_LEN = 8;
-   uint8_t msg[MSG_LEN];
    struct stMessage tMsg;
 
    // populate the message with integer values in binary format
-   tMsg.u8ID = (uint8_t)atoi(argv[1]);
-   tMsg.u8Task = (uint8_t)atoi(argv[2]);
-   tMsg.u16Addr = (uint16_t)atol(argv[3]);
-   tMsg.u16Msg = (uint16_t)atol(argv[4]);
+//    tMsg.u8ID = (uint8_t)atoi(argv[1]);
+//    tMsg.u8Task = (uint8_t)atoi(argv[2]);
+//    tMsg.u16Addr = (uint16_t)atol(argv[3]);
+//    tMsg.u16Msg = (uint16_t)atol(argv[4]);
 
-   msg[0] = tMsg.u8ID;
-   msg[1] = tMsg.u8Task;
-   msg[2] = (uint8_t)((tMsg.u16Addr & 0xFF00) >> 8);
-   msg[3] = (uint8_t)((tMsg.u16Addr & 0x00FF) >> 0);
-   msg[4] = (uint8_t)((tMsg.u16Msg & 0xFF00) >> 8);
-   msg[5] = (uint8_t)((tMsg.u16Msg & 0x00FF) >> 0);
+//    msg[0] = tMsg.u8ID;
+//    msg[1] = tMsg.u8Task;
+//    msg[2] = (uint8_t)((tMsg.u16Addr & 0xFF00) >> 8);
+//    msg[3] = (uint8_t)((tMsg.u16Addr & 0x00FF) >> 0);
+//    msg[4] = (uint8_t)((tMsg.u16Msg & 0xFF00) >> 8);
+//    msg[5] = (uint8_t)((tMsg.u16Msg & 0x00FF) >> 0);
    
    // from his notes: this number has low and high bytes swapped
    // hopefully this doesn't explode in the future ^^ 
-   tMsg.u16Crc = ModRTU_CRC(msg, 6);
+//    tMsg.u16Crc = ModRTU_CRC(msg, 6);
    // tMsg.u16Crc = 0x55aa;
+
+   tMsg.u8ID = send_msg.u8ID;
+   tMsg.u8Task = send_msg.u8Task;
+   tMsg.u16Addr = send_msg.u16Addr;
+   tMsg.u16Msg = send_msg.u16Msg;
+   tMsg.u16Crc = ModRTU_CRC(send_msg, 6);
 
    printf("Sent request: %02x %02x %04x %04x %04x\n", tMsg.u8ID, tMsg.u8Task, tMsg.u16Addr, tMsg.u16Msg, tMsg.u16Crc);
 
@@ -135,7 +139,7 @@ int sendMessage(int argc, char *argv[])
    return 0;
 }
 
-int recMessage(int argc, char *argv[], stMessage *rec_msg)
+int recMessage(stMessage *rec_msg)
 {
    int file, count;
 
@@ -173,10 +177,11 @@ int recMessage(int argc, char *argv[], stMessage *rec_msg)
    }
    else {
       printf("The following was read in [%d]: %02x %02x %02x%02x %02x%02x %02x%02x\n",count, rd_msg[0], rd_msg[1],rd_msg[2], rd_msg[3],rd_msg[4], rd_msg[5],rd_msg[6], rd_msg[7]);
+      *rec_msg = rd_msg;
    }
 
    close(file);
-   *rec_msg = rd_msg;
+   
    return 0;
 }
 
@@ -192,8 +197,6 @@ void handleError(stMessage rec_mes)
     }
 }
 
-
-
 int encodeMessage(stMessage *rec_msg, uint16_t *sensor_value)
 { 
     if(rec_msg.u8ID == 0x01) //Motor
@@ -202,36 +205,35 @@ int encodeMessage(stMessage *rec_msg, uint16_t *sensor_value)
         {
             // TODO: Handle Error
             printf("Error writing motor values. Rec msg: %d\n", rec_msg);
-            handleError(*rec_msg);
+            handleError(rec_msg);
             return -1;
         } // Error
+        else{
+            printf("Data successfully written\n");
+        }
         return 1;
 
     } else if (rec_msg.u8ID == 0x02) //Sensor
     {
         if((rec_msg.u8Task & 0xF0) == 0x80)
         {
-            // TODO: Handle Error
             printf("Error rec sensor values. Rec msg: %d\n", rec_msg);
-            handleError(*rec_msg);
+            handleError(rec_msg);
             return -1;
         } // Error
 
-        *sensor_value = (uint16_t) ((((uint16_t)rd_msg[4])  << 8) | (uint16_t)rd_msg[5]);
-
-        // easy cheat: we define the message directly, not dependent on any other bit
-        uint16_t rec_data = 0x0000;
-
+        if((rec_msg.u8Task & 0x0F) == 0x03)
+        {
+            *sensor_value = (uint16_t) ((((uint16_t)rd_msg[4])  << 8) | (uint16_t)rd_msg[5]);
+        } // Reading Task
+        else {
+            printf("Wrong Task code. Rec msg: %d\n", rec_msg);
+            return -1;
+        }
         return 1;
     }
 }
 
-// TODO: Define the way of the input
-// Ask Sensor for data
-// stMessage sensorMessage(stMessage message)
-// {
-    
-// }
 
 // // write Motor data
 // stMessage motorMessage(uint16_t reg, uint16_t data)
@@ -246,42 +248,43 @@ int encodeMessage(stMessage *rec_msg, uint16_t *sensor_value)
 // }
 
 
-
-int receiveTask()
-{
-    while (1)
-    {
-        stMessage received_message;
-
-        if ((count = read(fd, &received_message, sizeof(received_message)))<0)
-        {   //receive data
-            perror("Failed to read from the input\n");
-            return -1;
-        }
-        printf("Received bytes: [%d] counter value: %d\n",count, received_message);
-        // TODO: check for count -> is it a shorter message then wait for the rest
-
-        encodeMessage(received_message);
-    }
-
-    close(fd);
-    return 0;
-}
-
-
-
-
 int main(void)
 {
+    stMessage req_sensor = 0x02 0x03 0x0001 0x0001 0xFFFF; // initial sensor Message
+    stMessage write_motor = 0x01 0x06 0x0001 0x0001 0xFFFF; // initial motor Message
 
-    sendMessage(int argc, char *argv[])
-    // /* Threads for reading from Controller 2 and writing to Controller 1 */
-    // std::thread sendThread (sendTask);
-    // std::thread receiveThread (receiveTask);
+    stMessage *received_message;
+    uint16_t motorValue;
+    uint16_t *sensorValue;
+
+    while(true)
+    {
+        // Send Sensor Request   
+        sendMessage(req_sensor);
+        // Wait for Sensor Response
+        recMessage(received_message);
+        int ret = encodeMessage(received_message, sensorValue);
+
+        if(ret > 0)
+        {
+            // Interprete Sensor Data and then
+            motorValue = received_message->u16Msg;
+        }
+
+        // Send Motor Data
+        write_motor.u16Msg = motorValue;
+        sendMessage(write_motor);
+        // Wait for Motor Response
+        recMessage(received_message);
+        int ret = encodeMessage(received_message, sensorValue);
+
+        if(ret > 0)
+        {
+            // Message from Motor was okay
+            continue;
+        }
+    }
     
-    // /* Allow the tasks to run. */
-    // sendThread.join();
-    // receiveThread.join();
     
     return 0;
 }
